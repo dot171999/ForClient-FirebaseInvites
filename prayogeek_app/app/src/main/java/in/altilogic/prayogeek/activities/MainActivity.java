e488f3bae148462757d0604e9e69c509ff917709
Chetan Prasad <chetan@prayogeek.in>
2019-01-22 23:57:54 +0530

    - Implemented Firebase Login - Changed package name as it was not possible to connect to Firebase with the old Package name

---
+++ prayogeek_app/app/src/main/java/in/altilogic/prayogeek/activities/MainActivity.java
@@ -0,0 +1,234 @@
+package in.altilogic.prayogeek.activities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import in.altilogic.prayogeek.R;
+import in.altilogic.prayogeek.utils.Utils;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Firebase Authentication
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static final int RC_SIGN_IN = 3;
+
+    private String mUsername;
+    private String mEmailId;
+    public static final String ANONYMOUS = "anonymous";
+
+    public static final String PREF_USER_FIRST_TIME = "user_first_time";
+    boolean isUserFirstTime;
+    boolean isOnline = false;
+
+    public static final String TAG = "YOUSCOPE-DB";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        isUserFirstTime = Boolean.valueOf(Utils.readSharedSetting(MainActivity.this, PREF_USER_FIRST_TIME, "true"));
+
+        Intent introIntent = new Intent(MainActivity.this, OnBoardingActivity.class);
+        introIntent.putExtra(PREF_USER_FIRST_TIME, isUserFirstTime);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Firebase Authorization
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    // user is signed in
+                    mUsername = user.getDisplayName();
+                    mEmailId = user.getEmail();
+                    //Global_Var GlobalVar = (Global_Var) getApplicationContext() ;
+                    //GlobalVar.Set_Username(mUsername);
+                    //GlobalVar.Set_EmailId(mEmailId);
+                } else {
+                    // user is signed out
+                    mUsername = ANONYMOUS;
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false) // Smart lock automatically saves user credentials and logs in
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
+        if (isUserFirstTime)
+            startActivity(introIntent);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        isOnline = Utils.isOnline(this);
+        Toast.makeText(this, "Network available - " + isOnline, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                //sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        switch(requestCode)
+        {
+            case RC_SIGN_IN:
+                if(resultCode == RESULT_OK) {
+                    Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+                }else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Sign In Cancelled!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Uri uri = null;
+
+        if (id == R.id.nav_on_boarding) {
+            Intent intent = new Intent(this, OnBoardingActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.nav_basic_electronic_tutorial) {
+            Toast.makeText(this, "Run electronic tutorial", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_about_us) {
+            uri = Uri.parse(getString(R.string.nav_link_about_us));
+        } else if (id == R.id.nav_facebook) {
+            uri = Uri.parse(getString(R.string.nav_link_facebook));
+        } else if (id == R.id.nav_instagram) {
+            uri = Uri.parse(getString(R.string.nav_link_instagram));
+        } else if (id == R.id.nav_youtube_channel) {
+            uri = Uri.parse(getString(R.string.nav_link_youtube_channel));
+        } else if (id == R.id.nav_link_to_website) {
+            uri = Uri.parse(getString(R.string.nav_link_to_website));
+        } else if (id == R.id.nav_privacy_policy) {
+            Toast.makeText(this, "Privacy policy", Toast.LENGTH_SHORT).show();
+        }
+        if(uri != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
---
+++ prayogeek_app/app/src/main/java/in/altilogic/prayogeek/activities/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package in.altilogic.prayogeek.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(savedInstanceState == null)
+        {
+            super.onCreate(savedInstanceState);
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
