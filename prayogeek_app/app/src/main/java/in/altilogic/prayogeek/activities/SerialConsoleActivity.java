ef03e3585868039436c5b0ffe26533bedc84964d
Roman022 <RomanSablin.022@gmail.com>
2019-04-08 23:49:03 +0700

    moved the serial functionality to the fragment

--- prayogeek_app/app/src/main/java/in/altilogic/prayogeek/activities/SerialConsoleActivity.java
+++ prayogeek_app/app/src/main/java/in/altilogic/prayogeek/activities/SerialConsoleActivity.java
@@ -1,46 +1,30 @@
 package in.altilogic.prayogeek.activities;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.util.Log;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 import in.altilogic.prayogeek.R;
 import in.altilogic.prayogeek.fragments.SerialConsoleFragment;
 import in.altilogic.prayogeek.fragments.SerialConsoleSettingsFragment;
 import in.altilogic.prayogeek.service.ImageDownloadService;
 import in.altilogic.prayogeek.service.SerialConsoleService;
-import in.altilogic.prayogeek.utils.Utils;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class SerialConsoleActivity  extends AppCompatActivity implements View.OnClickListener, EasyPermissions.PermissionCallbacks {
     public static final String TAG = "YOUSCOPE-SERIAL";
 
     private FragmentManager mFragmentManager;
-    SerialConsoleFragment mConsoleFragment;
+    private SerialConsoleFragment mConsoleFragment;
     private final static int SCREEN_ID_SERIAL_SETTINGS = 14;
     private final static int SCREEN_ID_SERIAL_CONSOLE = 15;
 
@@ -65,94 +49,12 @@ public class SerialConsoleActivity  extends AppCompatActivity implements View.On
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        setFilters();
-        startService(SerialConsoleService.class, usbConnection, null);
-        Log.d(TAG, "onResume()");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        unregisterReceiver(mUsbReceiver);
-        unbindService(usbConnection);
-        Log.d(TAG, "onPause()");
-    }
-
-    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
-        if (!SerialConsoleService.SERVICE_CONNECTED) {
-            Intent startService = new Intent(this, service);
-            if (extras != null && !extras.isEmpty()) {
-                Set<String> keys = extras.keySet();
-                for (String key : keys) {
-                    String extra = extras.getString(key);
-                    startService.putExtra(key, extra);
-                }
-            }
-            startService(startService);
-        }
-        Intent bindingIntent = new Intent(this, service);
-        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    private void setFilters() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(SerialConsoleService.ACTION_USB_PERMISSION_GRANTED);
-        filter.addAction(SerialConsoleService.ACTION_NO_USB);
-        filter.addAction(SerialConsoleService.ACTION_USB_DISCONNECTED);
-        filter.addAction(SerialConsoleService.ACTION_USB_NOT_SUPPORTED);
-        filter.addAction(SerialConsoleService.ACTION_USB_PERMISSION_NOT_GRANTED);
-        filter.addAction(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME);
-        filter.addAction(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA);
-        registerReceiver(mUsbReceiver, filter);
-    }
-
-    @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btnSend:
-                writeSerial();
-            break;
             case R.id.btnConsoleSettings:
                 showSettingsFragment();
                 break;
-            case R.id.btnClear:
-            break;
-            case R.id.btnSave:
-            break;
         }
-    }
-
-    private void writeSerial() {
-        if(mConsoleFragment != null) {
-            String sendText = ((AutoCompleteTextView)mConsoleFragment.getView().findViewById(R.id.etSerialData)).getText().toString();
-            sendText += getNewLine();
-            byte[] bytes;
-            try {
-                bytes = sendText.getBytes("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                return;
-            }
-            Intent intent = new Intent(this, SerialConsoleService.class);
-            intent.putExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME, SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_WRITE_DATA);
-            intent.putExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA, bytes);
-            startService(intent);
-        }
-    }
-
-    private String getNewLine() {
-        int pos = Utils.readSharedSetting(getApplicationContext(), SerialConsoleSettingsFragment.SETTINGS_LINE_FEED, 0 );
-
-        if(pos == SerialConsoleService.SERIAL_LINE_FEED_CR)
-            return "\r";
-        else if(pos == SerialConsoleService.SERIAL_LINE_FEED_NL)
-            return "\n";
-        else if(pos == SerialConsoleService.SERIAL_LINE_FEED_NLCR)
-            return "\r\n";
-
-        return "";
     }
 
     private void showSettingsFragment() {
@@ -168,71 +70,6 @@ public class SerialConsoleActivity  extends AppCompatActivity implements View.On
         mFragmentManager.beginTransaction().replace(R.id.fragmentContent, mConsoleFragment).commit();
         checkWriteReadPermissions();
     }
-
-
-    /*
-     * Notifications from UsbService will be received here.
-     */
-    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.d(TAG, "BroadcastReceiver");
-
-            switch (Objects.requireNonNull(intent.getAction())) {
-                case SerialConsoleService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
-                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
-                    break;
-                case SerialConsoleService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
-                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
-                    break;
-                case SerialConsoleService.ACTION_NO_USB: // NO USB CONNECTED
-                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
-                    break;
-                case SerialConsoleService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
-                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
-                    break;
-                case SerialConsoleService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
-                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
-                    break;
-                case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME:
-                    int type = intent.getIntExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME, -1);
-                    switch(type){
-                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_PARAMETERS:
-                            break;
-                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_READ_DATA:
-                            byte[] serial_data = intent.getByteArrayExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA);
-                            long timestamp = intent.getLongExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_TIMESTAMP, 0);
-                            int color = intent.getIntExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_COLOR, 0);
-                            if(mConsoleFragment!= null && serial_data != null && mScreenStatus == SCREEN_ID_SERIAL_CONSOLE) {
-                                StringBuilder sb = new StringBuilder();
-                                boolean isHex = 0 < Utils.readSharedSetting(getApplicationContext(), SerialConsoleSettingsFragment.SETTINGS_DATA_FORMAT, 0 );
-
-                                String receive = new String(serial_data);
-
-                                sb      .append(Utils.getTimestamp(timestamp))
-                                        .append(isHex ? Utils.asciiToHex(receive) : receive );
-                                TextView tvConsole = ((TextView)mConsoleFragment.getView().findViewById(R.id.tvConsoleOut));
-                                if(tvConsole != null){
-                                    tvConsole.setTextColor(color);
-                                    String cStr = sb.toString();
-
-                                    tvConsole.append(Html.fromHtml("<font color="+color+">"+cStr+"</font>"));
-//                                    if(!isHex)
-                                    {
-                                        tvConsole.append("\r\n");
-                                    }
-                                    ((ScrollView)(mConsoleFragment.getView().findViewById(R.id.svConsole))).post(()->
-                                            ((ScrollView)(mConsoleFragment.getView().findViewById(R.id.svConsole))).fullScroll(View.FOCUS_DOWN));
-                                }
-                            }
-                            break;
-                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_WRITE_DATA:
-                            break;
-                    }
-                    break;
-            }
-        }
-    };
 
     @Override
     public void onBackPressed() {
@@ -256,24 +93,6 @@ public class SerialConsoleActivity  extends AppCompatActivity implements View.On
         }
         finish();
     }
-
-//    private SerialConsoleService usbService;
-
-    private final ServiceConnection usbConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
-//            usbService = ((SerialConsoleService.UsbBinder) arg1).getService();
-//            usbService.setHandler(mHandler);
-            Log.d(TAG, "onServiceConnected");
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            Log.d(TAG, "onServiceDisconnected");
-
-//            usbService = null;
-        }
-    };
 
     private boolean checkWriteReadPermissions() {
         String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
--- prayogeek_app/app/src/main/java/in/altilogic/prayogeek/fragments/SerialConsoleFragment.java
+++ prayogeek_app/app/src/main/java/in/altilogic/prayogeek/fragments/SerialConsoleFragment.java
@@ -1,25 +1,40 @@
 package in.altilogic.prayogeek.fragments;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Calendar;
+import java.io.UnsupportedEncodingException;
 import java.util.Objects;
+import java.util.Set;
 
 import in.altilogic.prayogeek.R;
+import in.altilogic.prayogeek.service.SerialConsoleService;
 import in.altilogic.prayogeek.utils.FileSaver;
 import in.altilogic.prayogeek.utils.Utils;
 
 
 public class SerialConsoleFragment extends Fragment implements AdapterView.OnClickListener  {
+    public static final String TAG = "YOUSCOPE-SERIAL-FR";
 
     private View.OnClickListener mListener;
     public void setOnClickListener(View.OnClickListener listener){
@@ -27,8 +42,8 @@ public class SerialConsoleFragment extends Fragment implements AdapterView.OnCli
     }
 
     private TextView mTvOut;
-    private Button btnSend, btnSettings, btnClear, btnSave;
     private ScrollView svConsole;
+    private AutoCompleteTextView etSerialData;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -39,11 +54,12 @@ public class SerialConsoleFragment extends Fragment implements AdapterView.OnCli
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         mTvOut = (TextView) view. findViewById(R.id.tvConsoleOut);
-        btnSend = (Button) view. findViewById(R.id.btnSend);
-        btnSettings = (Button) view. findViewById(R.id.btnConsoleSettings);
-        btnClear = (Button) view. findViewById(R.id.btnClear);
-        btnSave = (Button) view. findViewById(R.id.btnSave);
+        Button btnSend = (Button) view.findViewById(R.id.btnSend);
+        Button btnSettings = (Button) view.findViewById(R.id.btnConsoleSettings);
+        Button btnClear = (Button) view.findViewById(R.id.btnClear);
+        Button btnSave = (Button) view.findViewById(R.id.btnSave);
         svConsole = (ScrollView) view. findViewById(R.id.svConsole);
+        etSerialData = (AutoCompleteTextView) view. findViewById(R.id.etSerialData);
         btnSend.setOnClickListener(this);
         btnSettings.setOnClickListener(this);
         btnClear.setOnClickListener(this);
@@ -73,11 +89,173 @@ public class SerialConsoleFragment extends Fragment implements AdapterView.OnCli
                 FileSaver fileSaver = new FileSaver(Utils.readSharedSetting(getActivity(), SerialConsoleSettingsFragment.SETTINGS_LOG_FILE_NAME, getActivity().getPackageName()));
                 fileSaver.write(mTvOut.getText().toString());
                 mTvOut.setText("");
+                return;
             }
-            return;
+            case R.id.btnSend:
+                writeSerial();
+                break;
         }
 
         if(mListener != null)
             mListener.onClick(view);
     }
+
+    private boolean isHex() {
+        return 0 < Utils.readSharedSetting(getActivity().getApplicationContext(), SerialConsoleSettingsFragment.SETTINGS_DATA_FORMAT, 0 );
+    }
+
+
+    private void writeSerial() {
+        String sendText = etSerialData.getText().toString();
+        sendText += getNewLine();
+        byte[] bytes;
+        try {
+            bytes = sendText.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return;
+        }
+        Intent intent = new Intent(getActivity(), SerialConsoleService.class);
+        intent.putExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME, SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_WRITE_DATA);
+        intent.putExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA, bytes);
+        getActivity().startService(intent);
+    }
+
+    private String getNewLine() {
+        int pos = Utils.readSharedSetting(getActivity().getApplicationContext(), SerialConsoleSettingsFragment.SETTINGS_LINE_FEED, 0 );
+
+        if(pos == SerialConsoleService.SERIAL_LINE_FEED_CR)
+            return "\r";
+        else if(pos == SerialConsoleService.SERIAL_LINE_FEED_NL)
+            return "\n";
+        else if(pos == SerialConsoleService.SERIAL_LINE_FEED_NLCR)
+            return "\r\n";
+
+        return "";
+    }
+
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+//            usbService = ((SerialConsoleService.UsbBinder) arg1).getService();
+//            usbService.setHandler(mHandler);
+            Log.d(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            Log.d(TAG, "onServiceDisconnected");
+
+//            usbService = null;
+        }
+    };
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!SerialConsoleService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(getActivity(), service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            getActivity().startService(startService);
+        }
+        Intent bindingIntent = new Intent(getActivity(), service);
+        getActivity().bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if( isHex() ) {
+            etSerialData.setInputType(InputType.TYPE_CLASS_TEXT |
+                    InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        }
+        else {
+            etSerialData.setInputType(InputType.TYPE_CLASS_TEXT);
+        }
+        setFilters();
+        startService(SerialConsoleService.class, usbConnection, null);
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Objects.requireNonNull(getActivity()).unregisterReceiver(mUsbReceiver);
+        getActivity().unbindService(usbConnection);
+        Log.d(TAG, "onPause()");
+    }
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(SerialConsoleService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(SerialConsoleService.ACTION_NO_USB);
+        filter.addAction(SerialConsoleService.ACTION_USB_DISCONNECTED);
+        filter.addAction(SerialConsoleService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(SerialConsoleService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        filter.addAction(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME);
+        filter.addAction(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA);
+        getActivity().registerReceiver(mUsbReceiver, filter);
+    }
+
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (Objects.requireNonNull(intent.getAction())) {
+                case SerialConsoleService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case SerialConsoleService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case SerialConsoleService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case SerialConsoleService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case SerialConsoleService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+                case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME:
+                    int type = intent.getIntExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_NAME, -1);
+                    switch(type){
+                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_PARAMETERS:
+                            break;
+                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_READ_DATA:
+                            byte[] serial_data = intent.getByteArrayExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_DATA);
+                            long timestamp = intent.getLongExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_TIMESTAMP, 0);
+                            int color = intent.getIntExtra(SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_COLOR, 0);
+                            if(serial_data != null) {
+                                StringBuilder sb = new StringBuilder();
+                                boolean isHex = 0 < Utils.readSharedSetting(getActivity().getApplicationContext(), SerialConsoleSettingsFragment.SETTINGS_DATA_FORMAT, 0 );
+
+                                String receive = new String(serial_data);
+
+                                sb      .append(Utils.getTimestamp(timestamp))
+                                        .append(isHex ? Utils.asciiToHex(receive) : receive );
+                                if(mTvOut != null){
+                                    mTvOut.setTextColor(color);
+                                    String cStr = sb.toString();
+
+                                    mTvOut.append(Html.fromHtml("<font color="+color+">"+cStr+"</font>"));
+                                    mTvOut.append("\r\n");
+                                    svConsole.post(()-> svConsole.fullScroll(View.FOCUS_DOWN));
+                                }
+                            }
+                            break;
+                        case SerialConsoleService.SERIAL_SERVICE_MESSAGE_TYPE_WRITE_DATA:
+                            break;
+                    }
+                    break;
+            }
+        }
+    };
 }
+
